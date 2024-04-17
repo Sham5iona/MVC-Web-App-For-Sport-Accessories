ae520b6d03b7f38f1fa40dbfa4516936b4cca45f
Sham5iona <gom26@abv.bg>
2024-04-17 18:31:05 +0300

    Here is the model part which includes the all the tables to be created, all the columns in each one of them, use of encapsulation with private setters etc...!

---
+++ Sport Accessories/Sport Accessories/Models/BagProduct.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sport_Accessories.Models
+{
+    public class BagProduct
+    {
+        //Thats a mapping table between Bag and Product because they have a
+        //many-to-many relationship
+        public Bag Bag { get; set; }
+        private Guid _bag_id;
+        public Guid BagId
+        {
+            get { return _bag_id; }
+            private set { _bag_id = value; }
+        }
+
+        public Product Product { get; set; }
+        private Guid _product_id;
+        public Guid ProductId
+        {
+            get { return _product_id; }
+            private set { _product_id = value;}
+        }
+
+        //generate a value when the row is inserted or updated
+        //this column won't be touched in any way
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        private DateTime _last_modified_20118018;
+        public DateTime LastModified_20118018
+        {
+            get { return _last_modified_20118018; }
+            private set { _last_modified_20118018 = value; }
+        }
+
+        public BagProduct() //empty constructor for EFCore
+        {
+            
+        }
+
+    }
+}
---
+++ Sport Accessories/Sport Accessories/Models/Favourite.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Sport_Accessories.Areas.Identity.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sport_Accessories.Models
+{
+    public class Favourite
+    {
+        private Guid _favourite_id;
+        public Guid FavouriteId
+        {
+            get { return _favourite_id; }
+            private set { _favourite_id = value; }
+        }
+
+        public User User { get; set; }
+
+        //the user id is string because the id in the IdentityUser class is a string
+
+        private string _user_id;
+        public string UserId
+        {
+            get { return _user_id; }
+            private set { _user_id = value;}
+        }
+
+        public ICollection<ProductFavourite> ProductFavourites { get; set; }
+
+        //generate a value when the row is inserted or updated
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        private DateTime _updated_at;
+        public DateTime UpdatedAt
+        {
+            get { return _updated_at; }
+            private set { _updated_at = value; }
+        }
+
+        //generate a value when the row is inserted or updated
+        //this column won't be touched in any way
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        private DateTime _last_modified_20118018;
+        public DateTime LastModified_20118018
+        {
+            get { return _last_modified_20118018; }
+            private set { _last_modified_20118018 = value; }
+        }
+
+        public Favourite() //empty constructor for EFCore
+        {
+            
+        }
+
+        public Favourite(string user_id)
+        {
+            this.UserId = user_id;
+            this.ProductFavourites = new List<ProductFavourite>();
+        }
+    }
+}
