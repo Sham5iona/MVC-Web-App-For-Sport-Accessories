e4b0a8903fdc1a21950c830b5735526f25c20f81
Sham5iona <gom26@abv.bg>
2024-04-22 17:46:27 +0300

    Here are the services which inherit their interfaces or abstract classes! Inside are their BLLs. They follow the SOLID principles to demonstrate reliability and useability

---
+++ Sport Accessories/Sport Accessories/Services/AbstractProfilePicture.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Sport_Accessories.Areas.Identity.Models;
+
+namespace Sport_Accessories.Services
+{
+    public abstract class AbstractProfilePicture
+    {
+
+        protected string[] _allowed_types = { "image/jpeg", "image/png",
+                                            "image/jpg", "image/gif" };
+
+        public abstract Task<bool> ChangeProfilePictureAsync(IFormFile profilePicture,
+                                                             User user);
+        
+
+    }
+}
