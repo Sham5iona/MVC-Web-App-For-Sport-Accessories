af2988c8a1fb49e02feecdc32dde88ad152fba2e
Sham5iona <gom26@abv.bg>
2024-04-22 18:03:00 +0300

    Here the Program.cs has a lot of BLL implemented like adding more services, token provider, Identity and so on.

---
+++ Sport Accessories/Sport Accessories/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Sport_Accessories.Areas.Identity.Models;
+using Sport_Accessories.Data;
+using Sport_Accessories.Services;
+using Sport_Accessories.SettingModels;
+
+namespace Sport_Accessories
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddIdentity<User, IdentityRole>(options =>
+            {
+
+                options.SignIn.RequireConfirmedAccount = true;
+                options.Lockout.MaxFailedAccessAttempts = 23;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+
+
+            })
+                .AddDefaultTokenProviders()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //Add auto mapper
+            builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+            builder.Services.AddRazorPages();
+
+            builder.Services.Configure<SMTPSettings>(builder.Configuration
+                                                    .GetSection("SMTP"));
+
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+            builder.Services.AddTransient<ITwoFactorAuthentication,
+                                          TwoFactorAuthentication>();
+
+            builder.Services.AddScoped<UpdateProfilePicture>();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
