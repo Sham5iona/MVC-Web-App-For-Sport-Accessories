80e69dd93285753e1f5b52f9a2c129561adb80ab
Sham5iona <gom26@abv.bg>
2024-04-23 20:53:46 +0300

    In this commit, I have added additional logic for the html elements for example the hidden inputs in the change username functionality, the disable2FA one as well. Also added style for some html elements to prevent overflowing in the view!

---
+++ Sport Accessories/Sport Accessories/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿
+$("#submitButton").prop('disabled', true);
+$('#reset').prop('disabled', true);
+    $('#imageUpload').change(function () {
+        var input = this;
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imagePreview img').attr('src', e.target.result);
+            $("#submitButton").prop('disabled', false);
+            $('#reset').prop('disabled', false);
+        };
+
+        if (input.files && input.files[0]) {
+
+            reader.readAsDataURL(event.target.files[0]); // Convert to base64 string
+            
+
+        } else {
+            resetImagePreview();
+
+        }
+    });
+
+    $('#reset').click(function () {
+        resetImagePreview();
+        $('#imageUpload').val('');
+        $('#reset').prop('disabled', true);
+    });
+
+    // Function to reset the image preview
+    function resetImagePreview() {
+        $('#imagePreview img').attr('src', $('#originalImageUrl').val());
+        $("#submitButton").prop('disabled', true);
+}
+$('#ShowUsernameForm').click(function () {
+    $('#FormChangeUsername').toggle();
+});
+
+$('#ShowPasswordForm').click(function () {
+    $('#ChangePasswordForm').toggle();
+});
+
+
+//show the change username form for always when the span validation displays an error
+if ($('#usernameValidationError').text().trim() !== '') {
+    $('#FormChangeUsername').show();
+}
