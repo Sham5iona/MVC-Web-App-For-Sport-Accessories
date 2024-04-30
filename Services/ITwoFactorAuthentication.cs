44b760b6881c46b85d9795c7c71a73db91a0d8d8
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-04-30 17:42:18 +0300

    Changing the Viewes, creating more files in Services, wwwroot, ViewModels

---
+++ Services/ITwoFactorAuthentication.cs
@@ -0,0 +1,11 @@
+﻿using Sport_Accessories.Areas.Identity.Models;
+
+namespace Sport_Accessories.Services
+{
+    public interface ITwoFactorAuthentication
+    {
+        public Task<bool> Enable2FA(User current_user, string email);
+
+        public Task<bool> SendToken(User current_user);
+    }
+}
