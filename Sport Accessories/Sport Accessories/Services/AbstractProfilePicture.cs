bf47e02d4a213df5cfb7f4301e5cb45a9aedbf98
Sham5iona <gom26@abv.bg>
2024-05-02 23:32:24 +0300

    Adding additional logic to some services and creating others from scratch!

--- Sport Accessories/Sport Accessories/Services/AbstractProfilePicture.cs
+++ Sport Accessories/Sport Accessories/Services/AbstractProfilePicture.cs
@@ -6,11 +6,12 @@ namespace Sport_Accessories.Services
     public abstract class AbstractProfilePicture
     {
 
-        protected string[] _allowed_types = { "image/jpeg", "image/png",
+        public string[] Allowed_types = { "image/jpeg", "image/png",
                                             "image/jpg", "image/gif" };
 
         public abstract Task<bool> ChangeProfilePictureAsync(IFormFile profilePicture,
                                                              User user);
-        
+        public abstract Task<bool> AddProfilePictureAsync(
+                                            IFormFile? profilePicture, User user);
     }
 }
---
+++ Sport Accessories/Sport Accessories/Services/IOfferService.cs
@@ -0,0 +1,16 @@
+﻿using Sport_Accessories.Models;
+using Sport_Accessories.ViewModels;
+
+namespace Sport_Accessories.Services
+{
+    public interface IOfferService
+    {
+        public Task<Guid> GetCategoryIdAsync(string category_name);
+        public Task<bool> AddOfferAsync(Product product);
+
+        public Task<Guid> AddProductImageAsync(IFormFile? productFile, Photo photo);
+
+        public Task<bool> ChangeProductImageAsync(IFormFile? productFile, Product product);
+
+    }
+}
