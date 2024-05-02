ebeafe3019a49109106d8c78512238c10f31b151
Sham5iona <gom26@abv.bg>
2024-05-02 23:40:30 +0300

    Add more view models and validational data annotations!

---
+++ Sport Accessories/Sport Accessories/ViewModels/AdminLoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sport_Accessories.ViewModels
+{
+    public class AdminLoginViewModel
+    {
+        [Required(ErrorMessage = "Please, enter admin username!")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Please, enter admin password!")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please, confirm admin password!")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
---
+++ Sport Accessories/Sport Accessories/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sport_Accessories.ViewModels
+{
+    public class EditUserViewModel
+    {
+        [Required(ErrorMessage = "Please, enter username!")]
+        public string? UserName { get; set; }
+
+        public IFormFile? FormFile {  get; set; }
+
+        public string? FileName { get; set; }
+
+        [Required(ErrorMessage = "Please, enter an email address!")]
+        [EmailAddress(ErrorMessage = "Please, enter valid email address!")]
+        public string? Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public bool TwoFactorEnabled { get; set; }
+
+        public DateTimeOffset? LockoutEnd { get; set; }
+
+        public int? AccessFailedCount { get; set; }
+
+        [Required(ErrorMessage = "Please, enter valid password")]
+        [DataType(DataType.Password)]
+        public string? PasswordHash { get; set; }
+
+        public bool LockoutEnabled { get; set; }
+
+
+    }
+}
