464bbdd8d2fe00596e2d49b1bdcec0eb1343eca0
Sham5iona <gom26@abv.bg>
2024-05-13 00:17:03 +0300

    Here, I have added additional auto mappers for new classes!

--- Sport Accessories/Sport Accessories/Mapper/MappingProfile.cs
+++ Sport Accessories/Sport Accessories/Mapper/MappingProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Sport_Accessories.Areas.Identity.Models;
 using Sport_Accessories.Models;
 using Sport_Accessories.Services;
 using Sport_Accessories.ViewModels;
+using System.Security.Claims;
 
 namespace Sport_Accessories.Mapper
 {
@@ -12,23 +14,31 @@ namespace Sport_Accessories.Mapper
         {
 
             CreateMap<RegisterViewModel, User>()
+
                 .ForMember(dest => dest.FileName, opt =>
                 opt.MapFrom(src => "defaultImage.jpg"))
-                .ForMember(dest => dest.LastModified_20118018, opt =>
-                opt.MapFrom(src => DateTime.UtcNow));
+
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
 
             CreateMap<User, EditUserViewModel>();
 
             CreateMap<EditUserViewModel, User>()
                 .ForMember(dest => dest.FileName, opt => opt.Ignore())
 
-                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
 
 
             CreateMap<AddUserViewModel, User>()
                 //add temporary value to filename until the profile picture attaches
                 .ForMember(dest => dest.FileName, opt =>
-                opt.MapFrom(src => string.Empty));
+                opt.MapFrom(src => string.Empty))
+
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
 
             CreateMap<OfferViewModel, Product>()
 
@@ -40,7 +50,10 @@ namespace Sport_Accessories.Mapper
 
                 .ForMember(dest => dest.CategoryId, opt => opt.Ignore())
 
-                .ForMember(dest => dest.UserId, opt => opt.Ignore());
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
 
             CreateMap<Product, OfferViewModel>()
 
@@ -50,8 +63,38 @@ namespace Sport_Accessories.Mapper
                 .ForMember(dest => dest.FileName, opt =>
                 opt.MapFrom(src => src.Photo.FileName));
 
-            CreateMap<Product, Category>();
+            CreateMap<Product, Category>()
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
 
+            CreateMap<Category, CategoryViewModel>();
+
+            CreateMap<CategoryViewModel, Category>()
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
+
+            CreateMap<User, Bag>()
+                .ForMember(dest => dest.UserId, opt =>
+                opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
+
+            CreateMap<Product, BagProduct>()
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
+
+            CreateMap<Bag, BagProduct>()
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now));
+
+            CreateMap<AddAdminViewModel, User>()
+                .ForMember(dest => dest.LastModified_20118018, opt => opt
+                .MapFrom(src => DateTime.Now))
+
+                .ForMember(dest => dest.EmailConfirmed, opt =>
+                opt.MapFrom(src => true));
+
+            CreateMap<User, AddAdminViewModel>();
 
         }
     }
